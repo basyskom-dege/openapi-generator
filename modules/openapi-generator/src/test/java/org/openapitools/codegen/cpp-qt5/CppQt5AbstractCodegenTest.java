cbef5d6422d4228a4c44a99e70226b7026741f28
Marius Dege <marius.dege@basyskom.com>
2020-11-13 15:29:18 +0100

    added some more Tests

--- modules/openapi-generator/src/test/java/org/openapitools/codegen/cpp-qt5/CppQt5AbstractCodegenTest.java
+++ modules/openapi-generator/src/test/java/org/openapitools/codegen/cpp-qt5/CppQt5AbstractCodegenTest.java
@@ -19,12 +19,13 @@
 package org.openapitools.codegen.languages;
 
 import static org.openapitools.codegen.utils.StringUtils.*;
-
+import org.openapitools.codegen.languages.CppQt5AbstractCodegen;
+import org.openapitools.codegen.*;
 import org.graalvm.compiler.asm.Assembler.CodeAnnotation;
 import org.openapitools.codegen.CodegenConstants;
-import org.openapitools.codegen.languages.CppQt5AbstractCodegen;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod  ;
 import java.io.File;
 import jdk.jfr.Timestamp;
 import java.lang.String;
@@ -32,14 +33,21 @@ import java.util.*;
 import org.openapitools.codegen.utils.ModelUtils;
 import io.swagger.v3.oas.models.media.Schema;
 import io.swagger.v3.oas.models.OpenAPI;
-
+import static java.lang.System.*;
 
 
 public class CppQt5AbstractCodegenTest {
 
+    private CppQt5AbstractCodegen codegen;
+
+    @BeforeMethod
+    public void setUp() {
+        codegen = new CppQt5AbstractCodegen();
+    }
+
+
     @Test
     public void testContentCompressionEnabled() throws Exception {
-        final CppQt5AbstractCodegen codegen = new CppQt5AbstractCodegen();
         codegen.additionalProperties().put(CppQt5AbstractCodegen.CONTENT_COMPRESSION_ENABLED, true);
         codegen.processOpts();
 
@@ -50,7 +58,6 @@ public class CppQt5AbstractCodegenTest {
     
     @Test
     public void testToModelImport() throws Exception {
-        final CppQt5AbstractCodegen codegen = new CppQt5AbstractCodegen();
         codegen.processOpts();
         codegen.namespaces.put("string", "std::string");
         codegen.namespaces.forEach((k,v) -> Assert.assertEquals(codegen.toModelImport(k), "using "+ v + ";"));
@@ -69,7 +76,6 @@ public class CppQt5AbstractCodegenTest {
 
     @Test
     public void testToModelFilename() throws Exception {
-        final CppQt5AbstractCodegen codegen = new CppQt5AbstractCodegen();
         Assert.assertEquals(codegen.toModelFilename("testName"), camelize(codegen.getModelNamePrefix() + "_testName_" + codegen.getModelNameSuffix()));
 
     }
@@ -77,27 +83,123 @@ public class CppQt5AbstractCodegenTest {
     @Test
     public void testToGetTypeDeclaration() throws Exception {
 
-        //how to generate valid schemas?
-        final CppQt5AbstractCodegen codegen = new CppQt5ClientCodegen();
-        OpenAPI openAPI = new OpenAPI();
-        codegen.preprocessOpenAPI(openAPI);
-        codegen.processOpts();
-        List<Schema> m = new ArrayList<Schema>();
-        //this wont return any schema. 
-        ModelUtils.getAllSchemas(openAPI).forEach(schema -> {
-            m.add(schema);
-            codegen.getTypeDeclaration(schema);
+        //testing multiple yaml files to cover all schema types
+        OpenAPI openAPI = TestUtils.parseFlattenSpec("src/test/resources/3_0/petstore.yaml");
+        openAPI.getComponents().getSchemas().forEach((k,schema) -> {
+
+            Assert.assertNotEquals(codegen.getTypeDeclaration(schema), "");
         });
-        //check if there are any schemas
-        Assert.assertEquals(m.size(),0);
 
+        openAPI = TestUtils.parseFlattenSpec("src/test/resources/2_0/petstore-with-fake-endpoints-models-for-testing.yaml");
+        openAPI.getComponents().getSchemas().forEach((k,schema) -> {
+
+            Assert.assertNotEquals(codegen.getTypeDeclaration(schema), "");
+        });
+
+        openAPI = TestUtils.parseFlattenSpec("src/test/resources/3_0/form-multipart-binary-array.yaml");
+        openAPI.getComponents().getSchemas().forEach((k,schema) -> {
+
+            Assert.assertNotEquals(codegen.getTypeDeclaration(schema), "");
+        });
         
+        openAPI = TestUtils.parseFlattenSpec("src/test/resources/3_0/allOf.yaml");
+        openAPI.getComponents().getSchemas().forEach((k,schema) -> {
 
+            Assert.assertNotEquals(codegen.getTypeDeclaration(schema), "");
+        });
+        
+    }
+
+    @Test
+    public void testToDefaultValue() throws Exception {
+        
+        //testing multiple yaml files to cover all schema types (same as in "getTypeDeclaration" Test)
+        OpenAPI openAPI = TestUtils.parseFlattenSpec("src/test/resources/3_0/petstore.yaml");
+        openAPI.getComponents().getSchemas().forEach((k,schema) -> {
+
+            Assert.assertNotEquals(codegen.toDefaultValue(schema), "");
+        });
+
+        openAPI = TestUtils.parseFlattenSpec("src/test/resources/2_0/petstore-with-fake-endpoints-models-for-testing.yaml");
+        openAPI.getComponents().getSchemas().forEach((k,schema) -> {
+
+            Assert.assertNotEquals(codegen.toDefaultValue(schema), "");
+        });
+
+        openAPI = TestUtils.parseFlattenSpec("src/test/resources/3_0/form-multipart-binary-array.yaml");
+        openAPI.getComponents().getSchemas().forEach((k,schema) -> {
+
+            Assert.assertNotEquals(codegen.toDefaultValue(schema), "");
+        });
+        
+        openAPI = TestUtils.parseFlattenSpec("src/test/resources/3_0/allOf.yaml");
+        openAPI.getComponents().getSchemas().forEach((k,schema) -> {
+
+            Assert.assertNotEquals(codegen.toDefaultValue(schema), "");
+        });
+        
+    }   
+
+    @Test(description = "correctly produces imports with import mapping")
+    public void testPostProcessOperationsWithModelsTestWithImportMapping() {
+        //generate Import Mapping
+        final String importName = "QString";
+        Map<String, Object> operations = createPostProcessOperationsMapWithImportName(importName);
+        //generate Models. This wont give the model we need...
+        List<Object> allModels = new ArrayList<>();
+        OpenAPI openAPI = TestUtils.parseFlattenSpec("src/test/resources/3_0/petstore.yaml");
+        openAPI.getComponents().getSchemas().forEach((string,schema) -> {
+            allModels.add((Object)codegen.fromModel(string, schema));
+            });
+
+        //run Function to test with emptyList because CodegenModel cannot be casted to Map...
+        Map<String, Object> value = codegen.postProcessOperationsWithModels(operations, Collections.emptyList());
+        List<Map<String, Object>> extractedImports = (List<Map<String, Object>>) operations.get("imports");
+        
+        
+        Assert.assertEquals("[{import=QString, classname=testClass}, {import=#include \"testReturnBaseType.h\"}, {import=#include <QString>}]",value.get("imports").toString());
+        //Operations are too big to check...
+        Assert.assertNotEquals(value.get("operations").toString(),"");
+        Assert.assertEquals(value.size(), 2);
+    }
+    
+    private Map<String, Object> createPostProcessOperationsMapWithImportName(String importName) {
+        List<CodegenOperation> ops = new ArrayList<>();
+        //generate some operations
+        CodegenOperation e = new CodegenOperation();
+        e.returnBaseType = "testReturnBaseType";
+
+        //generate some parameters
+        List<CodegenParameter> allParams = new ArrayList<>();   
+        CodegenParameter p = new CodegenParameter();
+        p.baseType = "qint64";
+        p.isPrimitiveType = true;
+        allParams.add(p);
+        //add parameter to operations
+        e.allParams = allParams;
+        
+        //add to operations
+        ops.add(e);
+        
+        Map<String, Object> operations = new HashMap<String, Object>() {{
+            put("operation", ops);
+            put("classname", "testClass");
+        }};
+
+        Map<String, Object> importList = new HashMap<String, Object>() {{
+            put("import", importName);
+            put("classname", "testClass");
+        }};
+        List<Map<String, Object>> imports = new ArrayList<>();
+        imports.add(importList);
+        return new HashMap<String, Object>() {{
+            put("operations", operations);
+            put("imports", imports);
+        }};
     }
 
     @Test 
     public void testToVarName() throws Exception {
-        final CppQt5AbstractCodegen codegen = new CppQt5AbstractCodegen();
         //using toParamName once, to cover it. It just calls toVarName...
         Assert.assertEquals(codegen.toParamName("TESTSTRING"),"teststring");
         Assert.assertEquals(codegen.toVarName("camelTest"),"camel_test");   
@@ -107,7 +209,6 @@ public class CppQt5AbstractCodegenTest {
 
     @Test
     public void testNeedToImport() throws Exception {
-        final CppQt5AbstractCodegen codegen = new CppQt5AbstractCodegen();
         //using Boolean.TRUE leads to ambiguous Errors.
         Assert.assertEquals(codegen.needToImport("test"),true);
         Assert.assertEquals(codegen.needToImport(""),false);
@@ -118,33 +219,28 @@ public class CppQt5AbstractCodegenTest {
 
     @Test
     public void testEscapeQuotationMark() throws Exception {
-        final CppQt5AbstractCodegen codegen = new CppQt5AbstractCodegen();
         Assert.assertEquals(codegen.escapeQuotationMark("testStrWithQuotationMarks\"\""),"testStrWithQuotationMarks");
     }
 
 
     @Test
     public void testEcapeUnsafeCharacters() throws Exception {
-        final CppQt5AbstractCodegen codegen = new CppQt5AbstractCodegen();
         Assert.assertEquals(codegen.escapeUnsafeCharacters("testStrWithUnsafeChars*//*"),"testStrWithUnsafeChars*_//_*");
     }
 
     @Test
     public void testToApiName() throws Exception {
-        final CppQt5AbstractCodegen codegen = new CppQt5AbstractCodegen();
         Assert.assertEquals(codegen.toApiName("test"),codegen.getModelNamePrefix()+ codegen.getApiNamePrefix()+ "Test" + codegen.getApiNameSuffix());
     }
 
     @Test
     public void testToModelName() throws Exception {
-        final CppQt5AbstractCodegen codegen = new CppQt5AbstractCodegen();
         Assert.assertEquals(codegen.toModelName(null),codegen.getModelNamePrefix()+"UnknownModel");
         Assert.assertEquals(codegen.toModelName("bool"),"bool");
     }
 
     @Test
     public void testPreprocessOpenAPI() throws Exception {
-        final CppQt5AbstractCodegen codegen = new CppQt5AbstractCodegen();
         OpenAPI openAPI = new OpenAPI();
 
         codegen.preprocessOpenAPI(openAPI);
@@ -152,13 +248,11 @@ public class CppQt5AbstractCodegenTest {
 
     @Test
     public void testToEnumValue() throws Exception{
-        final CppQt5AbstractCodegen codegen = new CppQt5AbstractCodegen();
         Assert.assertEquals(codegen.toEnumValue("testString\n", "String"),"testString ");
     }
 
     @Test
     public void testisDataTypeString() throws Exception{
-        final CppQt5AbstractCodegen codegen = new CppQt5AbstractCodegen();
         Assert.assertEquals(codegen.isDataTypeString("QString"),true);
     }
 
